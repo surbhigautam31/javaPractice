d0385674a67bc6d57547b20cace2480379c15e29
surbhi gautam <surbhigauttam069@gmail.com>
2026-02-23 21:21:30 +0530

    Add files via upload

---
+++ ReversePyramid.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ReversePyramid{
+    public static void main(String[] agrs){
+        Scanner sc = new Scanner(System.in);
+        int n = 4;
+        int m = 4;
+        for(int i = n ; i>=1;i--){
+            for ( int j =1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
